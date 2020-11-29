b5915490fdde12998d45e9c7ecf6623043468a73
Justyn Chroł <justyn.chr1@gmail.com>
2020-11-29 17:22:09 +0100

    #5 Authentication first config

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
 
 /** @var Router $router */
 $router = app(Router::class);
 
 $router->middleware("auth:api")->get("/user", function (Request $request): User {
+    return $request->user();
+
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
