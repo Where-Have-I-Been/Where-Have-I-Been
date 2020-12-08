a9db3ac7aedebf013252fad9c5e040177d28eaeb
Justyn Chroł <justyn.chr1@gmail.com>
2020-12-08 11:04:40 +0100

    #5 Implementation of the service pattern

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticated;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+
+class User extends Authenticated
 {
     use HasApiTokens;
     use HasFactory;
