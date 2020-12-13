be0318ff4b1ffefe2d3e1e56cdb505bff7a4182d
Justyn Chroł <justyn.chr1@gmail.com>
2020-12-13 15:38:12 +0100

    #5 Improving validation

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -10,7 +10,7 @@ class RegisterRequest extends ApiRequest
     {
         return [
             "email" => [
-                "required",
+                "email:rfc,dns",
                 "unique:users",
             ],
             "password" => [
