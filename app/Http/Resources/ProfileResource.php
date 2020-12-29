dc03fa452d72145219fefc825d40587c1fe25f0b
Justyn Chroł <justyn.chr1@gmail.com>
2020-12-29 01:24:23 +0100

    #30 Refactored

--- app/Http/Resources/ProfileResource.php
+++ app/Http/Resources/ProfileResource.php
@@ -11,13 +11,15 @@ class ProfileResource extends JsonResource
     public function toArray($request): array
     {
         $country = $this->country;
+        $photo = $this->photo;
+
         return [
             "name" => $this->name,
-            "birth_date" => $this->when($this->birth_date !== null, $this->birth_date, null),
             "gender" => $this->gender,
-            "nationality" => $this->when($country !== null, new CountryResource($country), null),
             "description" => $this->description,
-            "photo" => $this->when($this->photo !== null, new PhotoResource($this->photo), null),
+            "birth_date" => $this->when($this->birth_date !== null, $this->birth_date, null),
+            "nationality" => $this->when($country !== null, new CountryResource($country), null),
+            "photo" => $this->when($photo !== null, new PhotoResource($photo), null),
         ];
     }
 }
