db6d448d1fca5ab14c371a31103c0f08eede43d9
Justyn Chroł <justyn.chr1@gmail.com>
2021-01-06 19:59:54 +0100

    #40 Migration bug fix

---
+++ database/migrations/2021_01_03_115845_update_photos.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePhotos extends Migration
+{
+
+    public function up()
+    {
+        Schema::table("photos", function (Blueprint $table): void {
+            $table->primary("id");
+        });
+    }
+
+
+    public function down()
+    {
+
+    }
+}
