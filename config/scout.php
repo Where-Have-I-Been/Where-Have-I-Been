6af3722ee1701c23ad97f8b49c77c56a710a1558
Justyn Chroł <justyn.chr1@gmail.com>
2021-01-11 10:57:19 +0100

    #49 Instaled scout & algolia

---
+++ config/scout.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'driver' => env('SCOUT_DRIVER', 'algolia'),
+    'prefix' => env('SCOUT_PREFIX', ''),
+    'queue' => env('SCOUT_QUEUE', true),
+    'after_commit' => false,
+    'chunk' => [
+        'searchable' => 500,
+        'unsearchable' => 500,
+    ],
+    'soft_delete' => false,
+    'identify' => env('SCOUT_IDENTIFY', true),
+    'algolia' => [
+        'id' => env('ALGOLIA_APP_ID', ''),
+        'secret' => env('ALGOLIA_SECRET', ''),
+    ],
+];
