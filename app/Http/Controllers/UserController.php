7a8971d2bed28a5f7d9623ec818becbddce90058
justyn <justyn.chr1@gmail.com>
2021-01-20 13:22:31 +0100

    #50 Added method in controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,8 @@ use App\Http\Requests\ChangePasswordRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use App\Services\Authentication\Password\PasswordServiceInterface;
+use App\Services\User\UserQueryString\Mapper\UserMapperInterface;
+use App\Services\User\UserServiceInterface;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -15,21 +17,33 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
-    private PasswordServiceInterface $service;
+    private PasswordServiceInterface $passwordService;
+    private UserServiceInterface $service;
 
-    public function __construct(PasswordServiceInterface $service)
+    public function __construct(PasswordServiceInterface $passwordService, UserServiceInterface $service)
     {
+        $this->passwordService = $passwordService;
         $this->service = $service;
     }
+
 
     public function show(Request $request): JsonResource
     {
         return new UserResource($request->user());
     }
 
+    public function index(Request $request, UserMapperInterface $mapperService)
+    {
+        $trips = $this->service->getUsers($mapperService->map(
+            $request->only("by-followings", "by-friends", "search-query")),
+            $request->user(),
+            $request->input("per-page"));
+        return UserResource::collection($trips);
+    }
+
     public function changePassword(User $user, ChangePasswordRequest $request): JsonResponse
     {
-        $this->service->changePassword($request->validated(), $user);
+        $this->passwordService->changePassword($request->validated(), $user);
 
         return response()->json([
             "message" => __("passwords.updated"),
