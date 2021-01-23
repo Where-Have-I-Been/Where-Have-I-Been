3f939eb538751cc9b5763c2e1afc5ffd6682494b
justyn <justyn.chr1@gmail.com>
2021-01-23 14:55:24 +0100

    #79 Bug fix

--- app/Services/User/Filter/UserFilterInterface.php
+++ app/Services/User/Filter/UserFilterInterface.php
@@ -6,9 +6,9 @@ namespace App\Services\User\Filter;
 
 use App\Models\User;
 use App\Services\User\UserQueryString\QueryStringData;
-use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 interface UserFilterInterface
 {
-    public function filterUsers(QueryStringData $data, User $user): Builder;
+    public function filterUsers(QueryStringData $data, User $user): MorphToMany;
 }
